f38f850b9a1c70a0dc2be33c403e13239e844eec
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-08-25 04:49:18 -0600

    Serving list of test clients

--- htmx+node/server.js
+++ htmx+node/server.js
@@ -6,6 +6,27 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// GET Users
+app.get("/users", (req, res) => {
+	const users = [
+		{ id: 1, name: "John Doe" },
+		{ id: 2, name: "Sherlok Holmes" },
+		{ id: 3, name: "Mary Jane" },
+	];
+
+	res.status(200).send(`
+		<h1 class="text-2xl font-bold my-4"> Users </h1>
+		<ul>
+			${users
+				.map(
+					(user) =>
+						`<li class='mx-0 my-0'>${user.id}: ${user.name}</li>`
+				)
+				.join("")}
+		</ul>
+	`);
+});
+
 app.listen(3000, () => {
 	console.log("Sever listening to port 3000");
 });
