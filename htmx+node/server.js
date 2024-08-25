d7d8d7d3b5201540986c0922e279640e3547359b
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-08-25 04:58:41 -0600

    Getting response from endpoint

--- htmx+node/server.js
+++ htmx+node/server.js
@@ -7,12 +7,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // GET Users
-app.get("/users", (req, res) => {
-	const users = [
-		{ id: 1, name: "John Doe" },
-		{ id: 2, name: "Sherlok Holmes" },
-		{ id: 3, name: "Mary Jane" },
-	];
+app.get("/users", async (req, res) => {
+	// const users = [
+	// 	{ id: 1, name: "John Doe" },
+	// 	{ id: 2, name: "Sherlok Holmes" },
+	// 	{ id: 3, name: "Mary Jane" },
+	// ];
+
+	const response = await fetch("https://jsonplaceholder.typicode.com/users");
+	const users = await response.json();
 
 	res.status(200).send(`
 		<h1 class="text-2xl font-bold my-4"> Users </h1>
