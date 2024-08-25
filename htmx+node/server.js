eb848b0c8fdb8bbd59c54dc553246b92184bf384
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-08-25 13:36:08 -0600

    Adding api handler for validating email

--- htmx+node/server.js
+++ htmx+node/server.js
@@ -144,6 +144,62 @@ app.post("/search/api", async (req, res) => {
 	}, 1000);
 });
 
+app.post("/contact/email", (req, res) => {
+	const email = req.body.email;
+
+	const emailRegex = /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
+
+	const isValid = {
+		message: "Email is valid",
+		class: "text-green-700",
+	};
+
+	const isInValid = {
+		message: "Email is invalid",
+		class: "text-red-700",
+	};
+
+	if (emailRegex.test(email)) {
+		res.send(`
+			<div class="mb-4" hx-target="this" hx-swap="outerHTML">
+				<label
+					class="block text-gray-700 text-sm font-bold mb-2"
+					for="email"
+					>Email Address</label
+				>
+				<input
+					name="email"
+					hx-post="/contact/email"
+					class="border rounded-lg py-2 px-3 w-full focus:outline-none focus:border-blue-500"
+					type="email"
+					id="email"
+					required
+				/>
+				<div class='${isValid.class}'>${isValid.message}</div>
+			</div>	
+		`);
+	} else {
+		res.send(`
+			<div class="mb-4" hx-target="this" hx-swap="outerHTML">
+				<label
+					class="block text-gray-700 text-sm font-bold mb-2"
+					for="email"
+					>Email Address</label
+				>
+				<input
+					name="email"
+					hx-post="/contact/email"
+					class="border rounded-lg py-2 px-3 w-full focus:outline-none focus:border-blue-500"
+					type="email"
+					id="email"
+					required
+				/>
+				<div class='${isInValid.class}'>${isInValid.message}</div>
+			</div>	
+		`);
+	}
+});
+
 app.listen(3000, () => {
 	console.log("Sever listening to port 3000");
 });
