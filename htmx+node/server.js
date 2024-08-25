1bab6d6be6d1c751f078e2e024d2b7ab5becebb6
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-08-25 05:12:30 -0600

    Adding request limit

--- htmx+node/server.js
+++ htmx+node/server.js
@@ -14,7 +14,11 @@ app.get("/users", async (req, res) => {
 	// 	{ id: 3, name: "Mary Jane" },
 	// ];
 
-	const response = await fetch("https://jsonplaceholder.typicode.com/users");
+	const limit = +req.query.limit || 10;
+
+	const response = await fetch(
+		`https://jsonplaceholder.typicode.com/users?_limit=${limit}`
+	);
 	const users = await response.json();
 
 	setTimeout(async () => {
