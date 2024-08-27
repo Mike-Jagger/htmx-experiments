c411345d8d74385eea2070a03fabfd3dd6b33152
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-08-27 10:30:43 -0600

    Printing all go primitive var types

--- go/test.go
+++ go/test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/cmplx"
 )
 
 func add(x, y int) int {
@@ -18,19 +19,59 @@ func split(sum int) (x, y int) {
 	return
 }
 
-var i, j int = 1, 2
+// var i, j int = 1, 2
+
+var (
+	Bool bool = true
+
+	String string = "string variable"
+	
+	Int int = 69
+	Int8 int8
+	Int16 int16
+	Int32 int32
+	Int64 int64
+
+	Uint uint
+	Uint8 uint8
+	Uint16 uint16
+	Uint32 uint32
+	Uint64 uint64 = 1<<64 - 1
+	Uintptr uintptr
+
+	Byte byte
+
+	Rune rune
+
+	FLoat32 float32
+	Float64 float64
+
+	Complex64 complex64
+	Complex128 complex128 = cmplx.Sqrt(-5 + 12i)
+)
+
+func PrintTypeNValue(_var interface{}) {
+	fmt.Printf("Type: %T Value: %v \n", _var, _var)
+}
 
 func main() {
-	var c, python = 1, true
-	java := "It is just Java bro, but with the dot:column declaration"
+	PrintTypeNValue(Bool)
+	PrintTypeNValue(String)
+	PrintTypeNValue(Int)
+	PrintTypeNValue(Uint64)
+	PrintTypeNValue(Complex128)
 
-	fmt.Println(add(1, 2))
-	
-	a, b := swap("Hello", "World!")
 
-	fmt.Println(a, b)
+	// var c, python = 1, true
+	// java := "It is just Java bro, but with the dot:column declaration"
 
-	fmt.Println(split(100))
+	// fmt.Println(add(1, 2))
 
-	fmt.Println(i, j, c, python, java)
+	// a, b := swap("Hello", "World!")
+
+	// fmt.Println(a, b)
+
+	// fmt.Println(split(100))
+
+	// fmt.Println(i, j, c, python, java)
 }
