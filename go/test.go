291dbcc258c1067bfe49eeb41a8822225f797179
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-08-27 09:43:53 -0600

    writing split functions to demonstrate use of naked return

--- go/test.go
+++ go/test.go
@@ -12,10 +12,18 @@ func swap(x, y string) (string, string) {
 	return y, x
 }
 
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
+
 func main() {
 	fmt.Println(add(1, 2))
 	
 	a, b := swap("Hello", "World!")
 
 	fmt.Println(a, b)
+
+	fmt.Println(split(100))
 }
