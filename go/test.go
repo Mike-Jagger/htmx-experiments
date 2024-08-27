07cb8a2672c8fd16511d5b93372312bd91440a03
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-08-27 09:21:06 -0600

    Creating swap functions that returns 2 values

--- go/test.go
+++ go/test.go
@@ -8,6 +8,14 @@ func add(x, y int) int {
 	return x + y
 }
 
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
 func main() {
 	fmt.Println(add(1, 2))
+	
+	a, b := swap("Hello", "World!")
+
+	fmt.Println(a, b)
 }
