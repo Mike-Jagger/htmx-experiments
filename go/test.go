a54790f5181d9a5a9683fcd5f5b48816b1e91ec4
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-08-29 18:52:01 -0600

    commenting out unused functions

--- go/test.go
+++ go/test.go
@@ -6,19 +6,19 @@ import (
 	"math/cmplx"
 )
 
-func add(x, y int) int {
-	return x + y
-}
+// func add(x, y int) int {
+// 	return x + y
+// }
 
-func swap(x, y string) (string, string) {
-	return y, x
-}
+// func swap(x, y string) (string, string) {
+// 	return y, x
+// }
 
-func split(sum int) (x, y int) {
-	x = sum * 4 / 9
-	y = sum - x
-	return
-}
+// func split(sum int) (x, y int) {
+// 	x = sum * 4 / 9
+// 	y = sum - x
+// 	return
+// }
 
 // var i, j int = 1, 2
 
