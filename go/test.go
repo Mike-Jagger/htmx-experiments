12cd20bcb338a491052edd38acbefc5ae80847e5
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-09-03 17:16:25 -0600

    Implementing concurrency with locks in go

--- go/test.go
+++ go/test.go
@@ -3,8 +3,7 @@ package main
 import (
 	"fmt"
 	"math/cmplx"
-
-	"golang.org/x/tour/tree"
+	"sync"
 )
 
 // func add(x, y int) int {
@@ -428,39 +427,153 @@ var (
 // 	}
 // }
 
-func Walk(tree *tree.Tree, ch chan int) {
-	if tree == nil {
-		return
-	}
+// func Walk(tree *tree.Tree, ch chan int) {
+// 	if tree == nil {
+// 		return
+// 	}
 	
-	Walk(tree.Left, ch)
-	ch <- tree.Value
-	Walk(tree.Right, ch)
+// 	Walk(tree.Left, ch)
+// 	ch <- tree.Value
+// 	Walk(tree.Right, ch)
+// }
+
+// func Same(t1, t2 *tree.Tree) bool {
+// 	ch1 := make(chan int)
+// 	ch2 := make(chan int)
+
+// 	go Walk(t1, ch1)
+// 	go Walk(t2, ch2)
+
+// 	for i := 0; i < 10; i++ {
+// 		// fmt.Println(<- ch1)
+// 		// fmt.Println(<- ch2)
+// 		n1 := <- ch1
+// 		n2 := <- ch2
+
+// 		if n1 != n2 {
+// 			return false
+// 		} 
+// 	}
+
+// 	return true
+// }
+
+
+type Fetcher interface {
+	// Fetch returns the body of URL and
+	// a slice of URLs found on that page.
+	Fetch(url string) (body string, urls []string, err error)
 }
 
-func Same(t1, t2 *tree.Tree) bool {
-	ch1 := make(chan int)
-	ch2 := make(chan int)
 
-	go Walk(t1, ch1)
-	go Walk(t2, ch2)
 
-	for i := 0; i < 10; i++ {
-		// fmt.Println(<- ch1)
-		// fmt.Println(<- ch2)
-		n1 := <- ch1
-		n2 := <- ch2
+// Crawl uses fetcher to recursively crawl
+// pages starting with url, to a maximum of depth.
+func Crawl(url string, depth int, fetcher Fetcher, cache *URLCache) {
+	// TODO: Fetch URLs in parallel.
+	// TODO: Don't fetch the same URL twice.
+	// This implementation doesn't do either:
 
-		if n1 != n2 {
-			return false
-		} 
+
+	if depth <= 0 {
+		return
 	}
 
-	return true
+	if !cache.inCache(url) {
+		cache.addURL(url)
+		body, urls, err := fetcher.Fetch(url)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Printf("found: %s %q\n", url, body)
+		for _, u := range urls {
+			Crawl(u, depth-1, fetcher, cache)
+		}
+	} else {
+		// fmt.Printf("\nAlready in cache: %v \n\n", url)
+	}
+}
+
+// fakeFetcher is Fetcher that returns canned results.
+type fakeFetcher map[string]*fakeResult
+
+type fakeResult struct {
+	body string
+	urls []string
+}
+
+func (f fakeFetcher) Fetch(url string) (string, []string, error) {
+	if res, ok := f[url]; ok {
+		return res.body, res.urls, nil
+	}
+	return "", nil, fmt.Errorf("not found: %s", url)
+}
+
+// fetcher is a populated fakeFetcher.
+var fetcher = fakeFetcher{
+	"https://golang.org/": &fakeResult{
+		"The Go Programming Language",
+		[]string{
+			"https://golang.org/pkg/",
+			"https://golang.org/cmd/",
+		},
+	},
+	"https://golang.org/pkg/": &fakeResult{
+		"Packages",
+		[]string{
+			"https://golang.org/",
+			"https://golang.org/cmd/",
+			"https://golang.org/pkg/fmt/",
+			"https://golang.org/pkg/os/",
+		},
+	},
+	"https://golang.org/pkg/fmt/": &fakeResult{
+		"Package fmt",
+		[]string{
+			"https://golang.org/",
+			"https://golang.org/pkg/",
+		},
+	},
+	"https://golang.org/pkg/os/": &fakeResult{
+		"Package os",
+		[]string{
+			"https://golang.org/",
+			"https://golang.org/pkg/",
+		},
+	},
+}
+
+type URLCache struct {
+	mu sync.Mutex
+	store map[string]bool
+}
+
+func (cache *URLCache) inCache(key string) bool {
+	cache.mu.Lock()
+	defer cache.mu.Unlock()
+
+	_, ok := (*cache).store[key]
+
+	return ok
+}
+
+func (cache *URLCache) addURL(key string) {
+	cache.mu.Lock()
+
+	((*cache).store)[key] = true
+	
+	cache.mu.Unlock()
 }
 
 func main() {
-	fmt.Println(Same(tree.New(1), tree.New(2)))
+	var customCache URLCache = URLCache{store: make(map[string]bool)}
+
+	Crawl("https://golang.org/", 4, fetcher, &customCache)
+
+	fmt.Println(customCache)
+
+	// fmt.Println(Same(tree.New(1), tree.New(2)))
 
 	// i := 5
 	// i_ptr := &i
