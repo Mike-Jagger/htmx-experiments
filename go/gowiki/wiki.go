2a00505df57341e5ae28bb46670d0649f407a3d6
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-09-04 12:14:32 -0600

    Adding function to handle edit requests to edit route for title

--- go/gowiki/wiki.go
+++ go/gowiki/wiki.go
@@ -40,9 +40,32 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>%s</h1><p>%s</p>", p.Title, p.Body)
 }
 
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+
+	fmt.Fprintf(w, 
+		"<h1>Editing: %s</h1>" +
+		"<form action='/save/%s' method='POST'>" +
+		"<textarea name='body'>%s</textarea><br>" +
+		"<input type='submit' value='Save'>" +
+		"</form>",
+		p.Title, p.Title, p.Body,
+	)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
 func main() {
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
 
 	fmt.Println("Server starting on port :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
