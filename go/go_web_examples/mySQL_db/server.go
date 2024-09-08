c8739b56d9e74c998d6108b12ca9c4b7242de33f
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-09-08 11:02:22 -0600

    done configuring docker mysql server

--- go/go_web_examples/mySQL_db/server.go
+++ go/go_web_examples/mySQL_db/server.go
@@ -3,11 +3,13 @@ package main
 import (
 	"database/sql"
 	"fmt"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 
 func main() {
-	db, err := sql.Open("mysql", "username:password@(127.0.0.1:3306)/dbname?parseTime=true")
+	db, err := sql.Open("mysql", "root:password@(localhost:3307)/htmx_experiments?parseTime=true")
 
 	if err != nil {
 		fmt.Println("Error configuring database connection:", err)
