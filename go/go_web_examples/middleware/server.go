6955ebfa0183ac9d54d684f8ebe37dfdafaffcdc
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-09-09 03:39:52 -0600

    playing with advanced middlewares

--- go/go_web_examples/middleware/server.go
+++ go/go_web_examples/middleware/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func logging(f http.HandlerFunc) http.HandlerFunc {
@@ -13,8 +14,20 @@ func logging(f http.HandlerFunc) http.HandlerFunc {
 	}
 }
 
+type MiddleWare func(http.HandlerFunc) http.HandlerFunc
+func Notify(logger *log.Logger, message string) MiddleWare {
+	return func(h http.HandlerFunc) http.HandlerFunc {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			logger.Println("Starting:", message)
+			defer logger.Println("Done:", message)
+			h(w, r)
+		})
+	}
+}
+
 func foo(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Foo")
+
 }
 
 func bar(w http.ResponseWriter, r *http.Request) {
@@ -25,11 +38,24 @@ func bazz(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("bazz")
 } 
 
+func handle(f http.HandlerFunc, middlewares ...MiddleWare) http.HandlerFunc {
+	for i := range middlewares {
+		f = middlewares[len(middlewares)-i-1](f)
+		// f = m(f) this will execute the functions in reverse order (stack) before executing first handlerfunc
+	}
+	return f
+
+}
+
 
 func main() {
+	logger := log.New(os.Stdout, "server: ", log.Lshortfile)
+
 	http.HandleFunc("/foo", logging(foo))
 	http.HandleFunc("/bar", logging(bar))
 	http.HandleFunc("/bazz", logging(bazz))
 
+	http.Handle("/", Notify(logger, "1")(Notify(logger, "2")(foo)))
+	http.Handle("/custom", handle(bar, Notify(logger, "1"), Notify(logger, "2")))
 	http.ListenAndServe(":80", nil)
 }	
