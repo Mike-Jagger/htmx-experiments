6cf960b55f2c3859b7121cafe40427c16a581be0
Mendjemo Gerard <88729231+Mike-Jagger@users.noreply.github.com>
2024-09-09 04:59:13 -0600

    Playing around with json encoding and decoding

--- go/go_web_examples/sessions_and_json/server.go
+++ go/go_web_examples/sessions_and_json/server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -45,10 +46,30 @@ func logout(w http.ResponseWriter, r *http.Request) {
 	session.Save(r, w)
 }
 
+type User struct {
+	FirstName string `json:"firstName"`
+	LastName string `json:"lastName"`
+	Age int `json:"age"`
+}
+
 func main() {
 	http.HandleFunc("/secret", secret)
 	http.HandleFunc("/login", login)
 	http.HandleFunc("/logout", logout)
+	http.HandleFunc("/encode", func(w http.ResponseWriter, r *http.Request) {
+		peter := User{
+			FirstName: "peter",
+			LastName: "parker",
+			Age: 25,
+		}
 
+		json.NewEncoder(w).Encode(peter)
+	})
+	http.HandleFunc("/decode", func(w http.ResponseWriter, r *http.Request) {
+		var user User
+		json.NewDecoder(r.Body).Decode(&user)
+
+		fmt.Fprintf(w, "%s %s is %d years old", user.FirstName, user.LastName, user.Age)
+	})
 	http.ListenAndServe(":80", nil)
 }
